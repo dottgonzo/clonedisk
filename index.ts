8d230f9bded7a372d89e01ae9399e736e3b62288
dario <darioyzf@gmail.com>
2016-02-05 00:02:54 +0100

    oki

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as pathExists from "path-exists";
 import * as child_process from "child_process";
 
 if(pathExists.sync("./dist/"+OS.platform()+"/"+OS.arch())){
-    child_process.exec("./dist/"+OS.platform()+"/"+OS.arch()+"/nw ./");
+    child_process.exec("sudo ./dist/"+OS.platform()+"/"+OS.arch()+"/nw ./");
 } else{
     throw Error("not supported platform");
 }
