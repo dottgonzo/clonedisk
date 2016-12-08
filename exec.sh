46954b500793aa8bb0ad33eb387ca90a18e9e56d
Dario Caruso <darioyzf@gmail.com>
2016-12-08 16:22:12 +0100

    fix bin exec

--- exec.sh
+++ exec.sh
@@ -1,6 +1,11 @@
-#! /bin/bash
+#!/usr/bin/env node
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+var child_process = require('child_process')
 
+child_process.exec('sudo nw '+__dirname,function(err,stdout,stderr){
 
-sudo nw DIR
+if (err) {
+console.log(err)
+}
+
+})
